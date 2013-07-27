9a383c3bc1190b22b96d9f84fca1a4778e2faa15
Bing Han, Goh <stiucsib86@hotmail.com>
2013-07-28 00:38:30 +0800

    HTTP_ORIGIN fixes.

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,6 @@
 <?php
 
-if (strpos($_SERVER['HTTP_HOST'], 'localhost') != -1) {
+if (strpos($_SERVER['HTTP_ORIGIN'], 'localhost') != -1) {
   $config['facebook_config']['app_id'] = '497232760353519';
   $config['facebook_config']['app_secret'] = 'ebafbea5bf5afc2d6961b0767e5e97a9';
 } else {
