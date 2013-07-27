fc79a7eb58c254bbc9226366aad88f0d5d1eb61c
Bing Han, Goh <stiucsib86@hotmail.com>
2013-07-27 23:22:03 +0800

    Added /company

--- application/config/tables/users.php
+++ application/config/tables/users.php
@@ -1,6 +1,7 @@
 <?php
 
 $config['tables']['users']['users']					= 'users';
-$config['tables']['users']['oauth_fb']				= 'users_oauth_fb';
+$config['tables']['users']['locations']			= 'users_locations';
+$config['tables']['users']['oauth_fb']      = 'users_oauth_fb';
 $config['tables']['users']['profile']				= 'users_profile';
 ?>
