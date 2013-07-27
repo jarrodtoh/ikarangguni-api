3fd61638062bca541721374ee4fca5eb6699207a
Bing Han, Goh <stiucsib86@hotmail.com>
2013-07-28 00:53:07 +0800

    Facebook App Config change.

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,10 +1,11 @@
 <?php
 
-if (strpos($_SERVER['HTTP_ORIGIN'], 'localhost') != -1) {
-  $config['facebook_config']['app_id'] = '497232760353519';
-  $config['facebook_config']['app_secret'] = 'ebafbea5bf5afc2d6961b0767e5e97a9';
-} else {
+if (isset($_SERVER['HTTP_ORIGIN']) && strpos($_SERVER['HTTP_ORIGIN'], 'localhost') != -1) {
   $config['facebook_config']['app_id'] = '155208588003403';
   $config['facebook_config']['app_secret'] = 'a447aca3a8349fae488ce76bbdaa6868';
+} else {
+  $config['facebook_config']['app_id'] = '497232760353519';
+  $config['facebook_config']['app_secret'] = 'ebafbea5bf5afc2d6961b0767e5e97a9';
 }
+
 ?>
