641663709785a6fbed0d53736e7f5e64997448a4
Bing Han, Goh <stiucsib86@hotmail.com>
2013-07-28 04:46:03 +0800

    Added /payout/issued

--- application/controllers/payout.php
+++ application/controllers/payout.php
@@ -37,6 +37,28 @@ class payout extends REST_Controller {
     }
   }
 
+  public function issued_get() {
+
+    try {
+      $fields = $this->_all_request_parameters;
+
+      if (!isset($fields['receiver_id'])) {
+        $fields['receiver_id'] = $this->session->userdata('user_id');
+      }
+
+      $results['balance'] = $this->notification_model->get_balance($fields);
+      $results['history'] = $this->notification_model->get_payout_history($fields);
+
+      $response_data['data'] = $results;
+      $this->response($response_data, 200);
+    } catch (Exception $e) {
+      $error_response = array();
+      $error_response['error'] = '[Error] ' . $e->getMessage();
+      $error_response['code'] = 404;
+      $this->response($error_response, 404);
+    }
+  }
+
 }
 
 ?>
